4d56aded945a1dfaa8f81923959b8f916252ecdd
Agung Panjimasjaya <69679429+apanji404@users.noreply.github.com>
2021-11-20 02:33:43 +0700

    Add files via upload

--- admin.php
+++ admin.php
@@ -33,7 +33,7 @@ require 'cek.php';
                             <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                             Dashboard
                         </a>
-                        <a class="nav-link" href="index.php">
+                        <a class="nav-link" href="stock.php">
                             <div class="sb-nav-link-icon"><i class="fas fa-box"></i></div>
                             Stock Barang
                         </a>
